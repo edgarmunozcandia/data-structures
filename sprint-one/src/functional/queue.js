f53980085579a8c51aff8f1b68d117844aa9db76
Edgar Munoz <edgarmunozcandia@gmail.com>
2018-01-12 10:15:49 -0600

    finished functional

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,29 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var first = 0;
+  var last = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[first] = value;
+    first++;
   };
 
   someInstance.dequeue = function() {
+    var totalCount = storage[last]
+    delete storage[last];
+    last++;
+    return totalCount;
   };
 
   someInstance.size = function() {
+    if(first - last > 0){
+      return first - last;
+    }else {
+      return 0;
+    }
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,26 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0;
+  var instance = {};
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[size] = value;
+    size++;
   };
 
   someInstance.pop = function() {
+    if(size > 0){
+      size--;
+      var value = storage[size];
+      delete storage[size];
+      return value;
+    }
   };
 
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;
